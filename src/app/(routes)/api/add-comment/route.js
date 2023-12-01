3e282e207963d6bc5b1632595ae0087a1529132a
02danieljonas <02danieljonas@gmail.com>
2023-11-30 21:08:07 -0500

    modified add-comment

--- src/app/(routes)/api/add-comment/route.js
+++ src/app/(routes)/api/add-comment/route.js
@@ -1,19 +1,22 @@
-import { sql } from '@vercel/postgres'
-import { NextResponse } from 'next/server'
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { posts_id, pets_username, comment_text } = await request.json()
+    const { searchParams } = new URL(request.url);
+    const postId = searchParams.get("postId");
+    const username = searchParams.get("username");
+    const comment = searchParams.get("username");
 
-  try {
-    if (!posts_id || !pets_username || !comment_text) {
-      throw new Error('Items required')
+    try {
+        if (!postId || !username || !comment) {
+        return NextResponse.json({ error: "Missing postId or username or comment" }, { status: 400 });
+        }
+
+        await sql`INSERT INTO Comments (posts_id, pets_username, comment_text) VALUES (${postId}, ${username}, ${comment});`;
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
-    await sql`INSERT INTO Comments (posts_id, pets_username, comment_text) VALUES (${posts_id}, ${pets_username}, ${comment_text});`
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 })
-  }
-
-  const posts = await sql`SELECT * FROM Posts;`
-  return NextResponse.json({ posts }, { status: 200 })
+    const posts = (await sql`SELECT * FROM Comments WHERE posts_id = ${postId};`).rows;
+    return NextResponse.json({ posts }, { status: 200 });
 }
