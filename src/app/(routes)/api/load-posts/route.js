8a6b2610f2abd3b97e5a078dee5adb47b9f30dec
Alejandro <alexvera0109@gmail.com>
2023-11-30 20:22:19 -0500

    load posts

--- src/app/(routes)/api/load-posts/route.js
+++ src/app/(routes)/api/load-posts/route.js
@@ -5,10 +5,17 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url)
   const username = searchParams.get('username')
   try {
+    const currentPet = await sql`SELECT * FROM Pets WHERE username = ${username};`
+    if (!currentPet || currentPet.rows.length === 0) {
+      return NextResponse.json({ error: 'Requesting from a false username' }, { status: 500 })
+    }
     const posts = await sql`
       SELECT p.id, p.pets_username, p.text, pet.petName, pet.petType,
       (SELECT COUNT(*) FROM Likes l WHERE l.posts_id = p.id) AS like_count,
-      EXISTS (SELECT 1 FROM Likes l WHERE l.posts_id = p.id AND l.pets_username = ${username}) AS liked_by_user
+      EXISTS (SELECT 1 FROM Likes l WHERE l.posts_id = p.id AND l.pets_username = ${username}) AS liked_by_user,
+      (SELECT JSON_AGG(json_build_object('id', c.id, 'pets_username', c.pets_username, 'comment_text', c.comment_text, 'created_at', c.created_at))
+      FROM Comments c 
+      WHERE c.posts_id = p.id) AS comments
       FROM Posts p
       JOIN Pets pet ON p.pets_username = pet.username;
     `
@@ -16,7 +23,6 @@ export async function GET(request) {
     return NextResponse.json({ allPosts }, { status: 200 })
   } catch (error) {
     const errorMessage = error.message
-    console.log(errorMessage)
     return NextResponse.json({ errorMessage }, { status: 500 })
   }
 }
